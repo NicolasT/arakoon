2ef38410b097082197a40a831d942221cb1b0da1
Romain Slootmaekers <romain@incubaid.com>
2013-05-17 16:32:54 +0200

    let's try this

--- src/system/drop_master.ml
+++ src/system/drop_master.ml
@@ -4,9 +4,10 @@ open Master_type
 open Node_cfg.Node_cfg
 
 
-let setup tn master base () = 
-  let lease_period = 10 in
-  let make_config () = Node_cfg.Node_cfg.make_test_config ~base 3 master lease_period in
+let setup tn master base () =
+  let p = 5.0 in
+  Lwt_unix.sleep p >>= fun () ->
+  let make_config () = Node_cfg.Node_cfg.make_test_config ~base 3 master (int_of_float p) in
   let t0 = Node_main.test_t make_config "t_arakoon_0" >>= fun _ -> Lwt.return () in
   let t1 = Node_main.test_t make_config "t_arakoon_1" >>= fun _ -> Lwt.return () in
   let t2 = Node_main.test_t make_config "t_arakoon_2" >>= fun _ -> Lwt.return () in
@@ -17,23 +18,23 @@ let teardown (tn, _, all_t) = Lwt.return ()
 
 let _with_master_admin (tn, cluster_cfg, _) f =
   let sp = float(cluster_cfg._lease_period) *. 1.2 in
-  Lwt_unix.sleep sp >>= fun () -> (* let the cluster reach stability *) 
+  Lwt_unix.sleep sp >>= fun () -> (* let the cluster reach stability *)
   Client_main.find_master cluster_cfg >>= fun master_name ->
   Lwt_log.info_f "master=%S" master_name >>= fun () ->
   let master_cfg =
-    List.hd 
+    List.hd
       (List.filter (fun cfg -> cfg.node_name = master_name) cluster_cfg.cfgs)
   in
   let host,port = List.hd master_cfg.ips , master_cfg.client_port in
   let sa = Network.make_address host port in
   let cid = cluster_cfg.cluster_id in
-  Lwt_io.with_connection sa 
+  Lwt_io.with_connection sa
     (fun conn ->
       Remote_nodestream.make_remote_nodestream cid conn >>= fun admin ->
       f cluster_cfg master_name admin
     )
 
-let _drop_master cluster_cfg master_name admin = 
+let _drop_master cluster_cfg master_name admin =
   Lwt_log.info "drop_master scenario" >>= fun () ->
   admin # drop_master () >>= fun () ->
   Client_main.find_master cluster_cfg >>= fun new_master ->
@@ -45,7 +46,7 @@ let drop_master tpl = _with_master_admin tpl _drop_master
 
 
 
-let make_suite base name w = 
+let make_suite base name w =
   let make_el n base f = n >:: w n base f in
   name >:::
     [make_el "drop_master" base drop_master;
