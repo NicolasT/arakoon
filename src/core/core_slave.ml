3e8322b29b1b9c631ccc1a7f7251a03492cf3f43
Nicolas Trangez <ikke@nicolast.be>
2013-07-08 01:55:40 +0200

    Extend slave handler

--- src/core/core_slave.ml
+++ src/core/core_slave.ml
@@ -14,6 +14,28 @@ let handle_election_timeout config state =
                    ] in
     (Candidate new_state, commands)
 
+let handle_prepare config state from m = failwith "Not implemented"
+
+let handle_promise config state from m =
+    let s = Printf.sprintf "Ignoring Promise from %S in Slave state" from in
+    (Slave state, [Command.Log s])
+
+let handle_accept config state from m = failwith "Not implemented"
+
+let handle_accepted config state from m =
+    let s = Printf.sprintf "Ignoring Accepted from %S in Slave state" from in
+    (Slave state, [Command.Log s])
+
+let handle_nop config state from =
+    (Slave state, [])
+
+let handle_message config state from = function
+  | Message.Prepare m -> handle_prepare config state from m
+  | Message.Promise m -> handle_promise config state from m
+  | Message.Accept m -> handle_accept config state from m
+  | Message.Accepted m -> handle_accepted config state from m
+  | Message.Nop -> handle_nop config state from
+
 let handle config state = function
   | Event.ElectionTimeout _ -> handle_election_timeout config state
-  | _ -> failwith "Not implemented"
+  | Event.Message (f, m) -> handle_message config state f m
