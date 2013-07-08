f4fe3c74550c52890b87c22b5c86f64dc4916669
Nicolas Trangez <ikke@nicolast.be>
2013-07-08 02:22:49 +0200

    Add some more 'implementation'

--- src/core/core_slave.ml
+++ src/core/core_slave.ml
@@ -1,4 +1,5 @@
 open Core_types
+open Core_types.Message
 
 open Core_state
 open Core_state.Slave
@@ -7,14 +8,19 @@ type t = Slave.t
 
 let handle_election_timeout config state =
     let next_n = N.succ state.n in
-    let msg = Message.prepare next_n in
+    let msg = prepare next_n in
     let new_state = Candidate.Fields.create ~n:next_n ~i:state.i
     and commands = [ Command.Log "Transition to Candidate"
                    ; Command.Broadcast msg
                    ] in
     (Candidate new_state, commands)
 
-let handle_prepare config state from m = failwith "Not implemented"
+let handle_prepare config state from = function
+  | m when m.Prepare.n < state.n ->
+      let s = Printf.sprintf !"Ignoring old Prepare for n %{N}, mine is %{N}"
+                  m.Prepare.n state.n in
+      (Slave state, [Command.Log s])
+  | _ -> failwith "Not implemented"
 
 let handle_promise config state from m =
     let s = Printf.sprintf "Ignoring Promise from %S in Slave state" from in
@@ -30,11 +36,11 @@ let handle_nop config state from =
     (Slave state, [])
 
 let handle_message config state from = function
-  | Message.Prepare m -> handle_prepare config state from m
-  | Message.Promise m -> handle_promise config state from m
-  | Message.Accept m -> handle_accept config state from m
-  | Message.Accepted m -> handle_accepted config state from m
-  | Message.Nop -> handle_nop config state from
+  | Prepare m -> handle_prepare config state from m
+  | Promise m -> handle_promise config state from m
+  | Accept m -> handle_accept config state from m
+  | Accepted m -> handle_accepted config state from m
+  | Nop -> handle_nop config state from
 
 let handle config state = function
   | Event.ElectionTimeout _ -> handle_election_timeout config state
