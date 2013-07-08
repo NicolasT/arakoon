1f82d9b1b6c7f6f4c2b95a354ac3ddb063dfe0e1
Nicolas Trangez <ikke@nicolast.be>
2013-07-08 02:25:57 +0200

    Namespace pollution

--- src/core/core_slave.ml
+++ src/core/core_slave.ml
@@ -1,4 +1,6 @@
 open Core_types
+open Core_types.Event
+open Core_types.Command
 open Core_types.Message
 
 open Core_state
@@ -10,8 +12,8 @@ let handle_election_timeout config state =
     let next_n = N.succ state.n in
     let msg = prepare next_n in
     let new_state = Candidate.Fields.create ~n:next_n ~i:state.i
-    and commands = [ Command.Log "Transition to Candidate"
-                   ; Command.Broadcast msg
+    and commands = [ Log "Transition to Candidate"
+                   ; Broadcast msg
                    ] in
     (Candidate new_state, commands)
 
@@ -19,18 +21,18 @@ let handle_prepare config state from = function
   | m when m.Prepare.n < state.n ->
       let s = Printf.sprintf !"Ignoring old Prepare for n %{N}, mine is %{N}"
                   m.Prepare.n state.n in
-      (Slave state, [Command.Log s])
+      (Slave state, [Log s])
   | _ -> failwith "Not implemented"
 
 let handle_promise config state from m =
     let s = Printf.sprintf "Ignoring Promise from %S in Slave state" from in
-    (Slave state, [Command.Log s])
+    (Slave state, [Log s])
 
 let handle_accept config state from m = failwith "Not implemented"
 
 let handle_accepted config state from m =
     let s = Printf.sprintf "Ignoring Accepted from %S in Slave state" from in
-    (Slave state, [Command.Log s])
+    (Slave state, [Log s])
 
 let handle_nop config state from =
     (Slave state, [])
@@ -43,5 +45,5 @@ let handle_message config state from = function
   | Nop -> handle_nop config state from
 
 let handle config state = function
-  | Event.ElectionTimeout _ -> handle_election_timeout config state
-  | Event.Message (f, m) -> handle_message config state f m
+  | ElectionTimeout _ -> handle_election_timeout config state
+  | Message (f, m) -> handle_message config state f m
